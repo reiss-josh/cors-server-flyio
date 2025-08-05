cac9b74392c5422b99c575a7f51fc571143376e2
reiss-josh <reiss.joshuae@gmail.com>
2025-08-05 12:14:17 -0700

    commit for cleanup

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,8 @@ const corsServer = corsAnywhere.createServer({
     originWhitelist: [
       'http://localhost:3000',
       'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
+      'https://api.steampowered.com/',
+      'https://store.steampowered.com',
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
